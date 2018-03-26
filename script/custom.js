3e8467a2c4d77bde7e24d60e7665733a152377af
Shenole Latimer <shenole@gmail.com>
2018-03-26 17:43:09 -0400

    Completed mobile device navigation. Only items left; (1) have banner receed on scroll (2) set up divs for insertion of roadmap and chatbot in homepage overlay.

--- script/custom.js
+++ script/custom.js
@@ -4,18 +4,32 @@
 let helpBtn = document.querySelector('#help-div');
 let helpModal = document.querySelector('#overlay');
 let xOut = document.querySelector('#close-icon');
+let burgNav = document.querySelector('#burger-nav-div');
+let burgMenu = document.querySelector('#mobi-nav-menu');
+let xOutNav = document.querySelector('#mobi-nav-icon');
 
+////////////////////////////
+// MOBILE NAVIGATION
+////////////////////////////
+burgNav.addEventListener('click', function() {
+  burgMenu.style.right = (0);
+});
+
+xOutNav.addEventListener('click', function() {
+  burgMenu.style.right = ('-100%');
+
+})
 
 ////////////////////////////
 // I NEED HELP BUTTON
 ////////////////////////////
 helpBtn.addEventListener('click', function() {
   helpModal.style.display = ('block')
-})
+});
 
 ////////////////////////////
 // I NEED HELP OVERLAY
 ////////////////////////////
 xOut.addEventListener('click', function() {
   helpModal.style.display = ('none');
-})
+});
