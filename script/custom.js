8bffc290841de9e6504a4ecdc3d09e14be146c53
Shenole Latimer <shenole@gmail.com>
2018-03-29 06:02:50 -0400

    morning update for shawn

--- script/custom.js
+++ script/custom.js
@@ -3,7 +3,7 @@
 ////////////////////////////
 let helpBtn = document.querySelector('#help-div');
 let helpModal = document.querySelector('#overlay');
-let xOut = document.querySelector('#close-icon');
+let xOut = document.querySelector('#exit-bot');
 let burgNav = document.querySelector('#burger-nav-div');
 let burgMenu = document.querySelector('#mobi-nav-menu');
 let xOutNav = document.querySelector('#mobi-nav-icon');
@@ -23,13 +23,12 @@ xOutNav.addEventListener('click', function() {
 ////////////////////////////
 // I NEED HELP BUTTON
 ////////////////////////////
-helpBtn.addEventListener('click', function() {
-  helpModal.style.display = ('block')
+helpBtn.addEventListener('click', function activeBot() {
+  document.querySelector('#chatbot-div').classList.toggle('rise');
+  if(document.querySelector('#chatbot-div').classList == 'rise') {
+    document.querySelector('#help-text').textContent = 'Close Chatbot';
+  } else (document.querySelector('#help-text').textContent = 'I Need Help')
 });
-
-function activeBot() {
-  document.querySelector('#chatbot-div').addClassList = ('rise');
-}
 
 ////////////////////////////
 // I NEED HELP OVERLAY
